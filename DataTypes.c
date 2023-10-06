b5c36e8c6eedb7af45f27efef94f071725470c83
samemajoya <107130927+samemajoya@users.noreply.github.com>
2023-10-06 13:23:39 +0600

    Add files via upload

---
+++ DataTypes.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main(){
+
+ int age = 25;
+ float salary = 5500.75;
+ char gender = 'F';
+
+ printf(" Age :%d\n" , age);
+ printf(" Salary: %.2f\n", salary);
+ printf("Gender: %c\n ", gender);
+
+ return 0;
+
+
+}
